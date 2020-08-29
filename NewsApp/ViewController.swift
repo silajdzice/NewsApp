2f2209d7615e8c57dce7c2baed5f067170359515
EdhemSilajdzic <30324025+silajdzice@users.noreply.github.com>
2020-08-29 16:01:41 +0200

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Edhem Silajdzic on 29/08/2020.
+//  Copyright © 2020 Edhem Silajdzic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
